10bdab65e5847f0514767d75c1b81245fe590b35
AnkitSainiYNR <ankit1825.be20@chitkara.edu.in>
2022-07-31 10:15:54 +0530

    Updates

---
+++ precache-manifest.46e0746d4b9d6a15d0cdd336444d46cb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f85fcacebd04dc83468",
+    "url": "/RobotFriends/static/css/main.602aea2d.chunk.css"
+  },
+  {
+    "revision": "8f85fcacebd04dc83468",
+    "url": "/RobotFriends/static/js/main.8f85fcac.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/RobotFriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "31dd50206303c7db6f21",
+    "url": "/RobotFriends/static/css/2.e26dd170.chunk.css"
+  },
+  {
+    "revision": "31dd50206303c7db6f21",
+    "url": "/RobotFriends/static/js/2.31dd5020.chunk.js"
+  },
+  {
+    "revision": "7ecd2fd7ffcce96fe9cb",
+    "url": "/RobotFriends/static/js/runtime~main.7ecd2fd7.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/RobotFriends/static/media/SEGA.82e09f78.TTF"
+  },
+  {
+    "revision": "272019c34033c5257f1762ca37436b84",
+    "url": "/RobotFriends/index.html"
+  }
+];
